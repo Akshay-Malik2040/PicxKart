d49941db4e8ce97aeed6a4f48b310b6e6ead4470
Akshay-Malik2040 <akshaymalik2040@gmail.com>
2025-09-27 23:43:37 +0530

    merge pixabay with pexels footage

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState,useRef } from 'react'
 
 
 const Body = () => {
-    const apiKey = import.meta.env.VITE_PEXELS_API;
+    const pexelsKey = import.meta.env.VITE_PEXELS_API;
+    const pixabayKey=import.meta.env.VITE_PIXABAY_API;
+
     const [imageList,setImageList]=useState([]);
     const [page,setPage]=useState(1);
     const [loading,setLoading]=useState(false);
@@ -12,28 +14,51 @@ const Body = () => {
 
     const fetchData= async()=>{
         setLoading(true);
-        const data=await fetch(`https://api.pexels.com/v1/curated?per_page=12&page=${page}`, {
+        const [pexelsData,pixabayData]=await Promise.all([fetch(`https://api.pexels.com/v1/curated?per_page=12&page=${page}`, {
         headers: {
-          Authorization: apiKey,
+          Authorization: pexelsKey,
         },
-      })
-      const ImgList=await data.json();
-     setImageList(prev => {
-        const newPhotos = ImgList.photos.filter(
+      }),
+    fetch(`https://pixabay.com/api/?key=${pixabayKey}&per_page=12&page=${page}`)])
+      
+    const pexelsList=await pexelsData.json();
+    const pixabayList= await pixabayData.json();
+
+    const pexelsFootageList=pexelsList.photos.map(photo=>({
+      id:`pexels-${photo.id}`,
+      url:photo.src.original
+    }))
+
+    const pixabayFootageList=pixabayList.hits.map(photo=>({
+      id:`pixabay-${photo.id}`,
+      url:photo.largeImageURL
+    }))
+    
+    // console.log(pexelsFootageList)
+    // console.log(pixabayList)
+
+    const merged=[...pexelsFootageList,...pixabayFootageList]
+
+    setImageList(prev => {
+        const newPhotos = merged.filter(
           photo => !prev.some(p => p.id === photo.id)
         );
         return [...prev, ...newPhotos];
       });
-      console.log(ImgList);
+
+    //  setImageList(prev => {
+    //     const newPhotos = ImgList?.photos?.filter(
+    //       photo => !prev.some(p => p.id === photo.id)
+    //     );
+    //     return [...prev, ...newPhotos];
+    //   });
+    //   console.log(ImgList);
       setLoading(false);
     }
-
-
 
     useEffect(()=>{
         fetchData();
     },[page])
-
 
     //Intersection Observer
    useEffect(() => {
@@ -61,7 +86,7 @@ const Body = () => {
         <h1 className='font-bold text-4xl mx-2 mb-4 px-2'>Free Stock Footage</h1>
         <div className='w-full columns-4 gap-6 p-4'>
             {
-                imageList?.map(photo=>(<img src={photo?.src?.large} key={photo?.id} className='w-full mb-8 h-auto object-cover break-inside-avoid rounded-xl'></img>))
+                imageList?.map(photo=>(<img src={photo?.url} key={photo?.id} className='w-full mb-8 h-auto object-cover break-inside-avoid rounded-xl'></img>))
             }
         </div>
         <div ref={loader} className="h-10 w-full flex justify-center items-center">
